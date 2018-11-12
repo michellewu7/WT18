59950183e9e2c0aa28495ddad3d48846ef17e669
michellewu7 <44569134+michellewu7@users.noreply.github.com>
2018-11-12 13:41:50 +0100

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Teilnehmer_innen WebTech</title>
+</head>
+<body>
+<header>
+Teilnehmer_innen WebTech
+</header>
+<img src="index.jpg" height="48" width="53"/>
+</nav>
+<section>
+Ale Teilnehmer_innen
+<table>
+<tr>
+<th>Vorname</th>
+<th>Nachname</th>
+<th>E-Mail-Adresse</th>
+<th>IP-Adresse</th>
+</tr>
+<tr>
+...
+</table>
+</section>
+<footer>
+Michelle Wu
+</footer>
+</body>
+</html>
